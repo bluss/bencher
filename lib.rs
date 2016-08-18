461ec115bfdba730fe26840f62e7ce47ff813730
bluss <bluss@users.noreply.github.com>
2016-08-18 11:11:13 +0200

    Remove black_box, since it's not been implemented in stable rust

--- lib.rs
+++ lib.rs
@@ -747,24 +747,15 @@ impl MetricMap {
 
 // Benchmarking
 
+// FIXME: We don't have black_box in stable rust
+
 /// A function that is opaque to the optimizer, to allow benchmarks to
 /// pretend to use outputs to assist in avoiding dead-code
 /// elimination.
 ///
 /// This function is a no-op, and does not even read from `dummy`.
-#[cfg(not(any(all(target_os = "nacl", target_arch = "le32"),
-              target_arch = "asmjs")))]
-pub fn black_box<T>(dummy: T) -> T {
-    // we need to "use" the argument in some way LLVM can't
-    // introspect.
-    //unsafe { asm!("" : : "r"(&dummy)) }
-    dummy
-}
-#[cfg(any(all(target_os = "nacl", target_arch = "le32"),
-          target_arch = "asmjs"))]
-#[inline(never)]
-pub fn black_box<T>(dummy: T) -> T {
-    dummy
+fn black_box<T>(dummy: T) -> T {
+   dummy
 }
 
 
